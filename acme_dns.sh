e659d748ed870218917074ebab0414572c23d58c
mritd <mritd1234@gmail.com>
2016-10-02 18:10:43 +0800

    add echo

--- acme_dns.sh
+++ acme_dns.sh
@@ -13,18 +13,29 @@ if [ "$GD_Secret" == "" ];then
   exit 1
 fi
 
+echo -e "\033[32mSystem updateing.......\033[0m"
 yum update -y 
 
+echo -e "\033[32mInstall nc and crontabs......\033[0m"
 yum install -y nc crontabs
 
-curl  https://get.acme.sh | bash
+if ! $? ;then
+  echo -e "\033[31mInstall nc or crontabs failed, System exiting!\033[0m"
+  exit 1
+fi
 
+echo -e "\033[32mInstall acme.sh.......\033[0m"
+curl https://get.acme.sh | bash
+
+echo -e "\033[32mCreate SSL CRT.......\033[0m"
 ~/.acme.sh/acme.sh   --issue   --dns dns_GD -d mritd.me -d www.mritd.me -d cdn.mritd.me
 
+echo -e "\033[32mCopy SSL CRT......\033[0m"
 ~/.acme.sh/acme.sh  --installcert -d mritd.me -d www.mritd.me \
                     --keypath /etc/nginx/ssl/mritd.me.key \
                     --certpath /etc/nginx/ssl/mritd.me.cer \
                     --reloadcmd "cd /root/docker/mritd && docker-compose restart" 
 
+echo -e "\033[32mUpdate acme.sh......\033[0m"
 ~/.acme.sh/acme.sh  --upgrade  --auto-upgrade
 
