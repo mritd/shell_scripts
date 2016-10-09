9a83d45e955c32a5b0b48cb6fa8679ce8802f748
mritd <mritd@mritd.me>
2016-10-09 16:19:28 +0800

    删除 kubebin 下载脚本

--- download_kubebin.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-kubeVersion=v1.3.5
-
-mkdir kubebin 
-for binName in kubectl kube-dns hyperkube kubemark kube-apiserver kube-controller-manager kube-scheduler kube-proxy kubelet;do
-  wget https://storage.googleapis.com/kubernetes-release/release/$kubeVersion/bin/linux/amd64/$binName -O kubebin/$binName
-  chmod +x kubebin/$binName
-done
