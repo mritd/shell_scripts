eef9470f9010e7c33b2456ee675805185e7b6fef
mritd <mritd@mritd.me>
2016-10-09 11:50:37 +0800

    修复数组遍历问题

--- download_kube_container.sh
+++ download_kube_container.sh
@@ -2,7 +2,7 @@
 
 containers=(kube-proxy-amd64:v1.4.0 kube-discovery-amd64:1.0 kubedns-amd64:1.7 kube-scheduler-amd64:v1.4.0 kube-controller-manager-amd64:v1.4.0 kube-apiserver-amd64:v1.4.0 etcd-amd64:2.2.5 kube-dnsmasq-amd64:1.3 exechealthz-amd64:1.1 pause-amd64:3.0)
 
-for containerName in containers ; do
+for containerName in ${containers[@]} ; do
   echo -e "\033[32mpull image: $containerName...\033[0m"
   docker pull $containerName
   if $?; then
