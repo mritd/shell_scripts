59941e2e83852819d94661fdbedfa66aca40b690
mritd <mritd@mritd.me>
2016-10-09 11:58:02 +0800

    merge

--- download_kube_container.sh
+++ download_kube_container.sh
@@ -4,11 +4,11 @@ containers=(kube-proxy-amd64:v1.4.0 kube-discovery-amd64:1.0 kubedns-amd64:1.7 k
 
 for containerName in ${containers[@]} ; do
   echo -e "\033[32mpull image: $containerName...\033[0m"
-  docker pull $containerName
+  docker pull gcr.io/google_containers/$containerName
   if $?; then
     echo -e "\033[31merror: pull image: $containerName failed!"
     exit 1
   fi
   echo -e "\033[32msave image: $containerName..."
-  docker save $containerName > $containerName.tar
+  docker save gcr.io/google_containers/$containerName > $containerName.tar
 done
