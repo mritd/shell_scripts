b78335c911c4edb8a7cee9ea61246838cbda67f9
mritd <mritd@mritd.me>
2016-10-09 11:40:38 +0800

    增加 kubenetes 相关 container 下载脚本

---
+++ download_kube_container.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+containers=(kube-proxy-amd64:v1.4.0 kube-discovery-amd64:1.0 kubedns-amd64:1.7 kube-scheduler-amd64:v1.4.0 kube-controller-manager-amd64:v1.4.0 kube-apiserver-amd64:v1.4.0 etcd-amd64:2.2.5 kube-dnsmasq-amd64:1.3 exechealthz-amd64:1.1 pause-amd64:3.0)
+
+for containerName in containers ; do
+  echo -e "\033[32mpull image: $containerName...\033[0m"
+  docker pull $containerName
+  if $?; then
+    echo -e "\033[31merror: pull image: $containerName failed!"
+    exit 1
+  fi
+  echo -e "\033[32msave image: $containerName..."
+  docker save $containerName > $containerName.tar
+done
