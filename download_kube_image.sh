0af5ef06b8e683903a6f007760adf8a45f1ed3e1
mritd <mritd1234@gmail.com>
2016-10-09 23:33:31 +0800

    更新变量名称

--- download_kube_image.sh
+++ download_kube_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-containers=(kube-proxy-amd64:v1.4.0 kube-discovery-amd64:1.0 kubedns-amd64:1.7 kube-scheduler-amd64:v1.4.0 kube-controller-manager-amd64:v1.4.0 kube-apiserver-amd64:v1.4.0 etcd-amd64:2.2.5 kube-dnsmasq-amd64:1.3 exechealthz-amd64:1.1 pause-amd64:3.0)
+images=(kube-proxy-amd64:v1.4.0 kube-discovery-amd64:1.0 kubedns-amd64:1.7 kube-scheduler-amd64:v1.4.0 kube-controller-manager-amd64:v1.4.0 kube-apiserver-amd64:v1.4.0 etcd-amd64:2.2.5 kube-dnsmasq-amd64:1.3 exechealthz-amd64:1.1 pause-amd64:3.0)
 
 echo -e "\033[33mclean old files!\033[0m"
 rm -rf ~/kube_images > /dev/null 2>&1
@@ -9,20 +9,20 @@ rm -f ~/kube_images.tar.gz > /dev/null 2>&1
 echo -e "\033[32mcreate download directory...\033[0m"
 mkdir ~/kube_images
 
-for containerName in ${containers[@]} ; do
-  echo -e "\033[32mpull image: $containerName...\033[0m"
-  docker pull gcr.io/google_containers/$containerName
+for imageName in ${images[@]} ; do
+  echo -e "\033[32mpull image: $imageName...\033[0m"
+  docker pull gcr.io/google_containers/$imageName
   if [ ! "$?"=="0" ]; then
-    echo -e "\033[31merror: pull image: $containerName failed!\033[0m"
+    echo -e "\033[31merror: pull image: $imageName failed!\033[0m"
     exit 1
   fi
-  echo -e "\033[32msave image: $containerName...\033[0m"
-  docker save gcr.io/google_containers/$containerName > ~/kube_images/$containerName.tar
+  echo -e "\033[32msave image: $imageName...\033[0m"
+  docker save gcr.io/google_containers/$imageName > ~/kube_images/$imageName.tar
 
-  if [ -f ~/kube_images/$containerName.tar ]; then
-    echo -e "\033[32mdownload $containerName image success!\033[0m"
+  if [ -f ~/kube_images/$imageName.tar ]; then
+    echo -e "\033[32mdownload $imageName image success!\033[0m"
   else
-    echo -e "\033[31mdownload $containerName image failed!\033[0m"
+    echo -e "\033[31mdownload $imageName image failed!\033[0m"
     exit 1
   fi
 done
