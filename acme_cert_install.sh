cf9eec0104679d35162b0e1382a24a22d3bfdf73
mritd <mritd@mritd.me>
2016-10-10 13:36:58 +0800

    兼容 zsh

--- acme_cert_install.sh
+++ acme_cert_install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-/usr/bin/read -p "请输入 keypath 目录:" keypath
-/usr/bin/read -p "请输入 certpath 目录:" certpath
-/usr/bin/read -p "请输入域名(多个以空格分割):" domains
-/usr/bin/read -p "请输入重载命令(非必选):" reloadcmd
+shellName=`ps | grep $$ | awk '{print $4}'`
+
+if [ "$shellName" == "zsh" ]; then
+  read "keypath?请输入 keypath 目录: "
+  read "certpath?请输入 certpath 目录: "
+  read "domains?请输入域名(多个以空格分割): "
+  read "reloadcmd?请输入重载命令(非必选): "
+else
+  read -p "请输入 keypath 目录: " keypath
+  read -p "请输入 certpath 目录: " certpath
+  read -p "请输入域名(多个以空格分割) :" domains
+  read -p "请输入重载命令(非必选): " reloadcmd
+fi
 
 if [ "$keypath" == "" ] ||  [ "$certpath" == "" ] ||  [ "$domains" == "" ]; then
   echo -e "\033[31merror: keypath、certpath or domains is blank!"
