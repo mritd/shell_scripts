f06a9e6a2cdfc9af65af5c326701d71c218f9a71
mritd <mritd@mritd.me>
2016-10-10 12:33:30 +0800

    分离安装步骤

---
+++ acme_cert_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+read keypath -p "请输入 keypath 目录:"
+read certpath -p "请输入 certpath 目录:"
+read domains -p "请输入域名(多个以空格分割):"
+read reloadcmd -p "请输入重载命令(非必选):"
+
+if [ "$keypath" == "" ] ||  [ "$certpath" == "" ] ||  [ "$domains" == "" ]; then
+  echo -e "\033[31merror: keypath、certpath or domains is blank!"
+  exit 1
+fi
+
+allDomian=($domains)
+
+for domainName in ${allDomian[@]}; do
+  installcert+=" -d $domainName"
+done
+
+~/.acme.sh/acme.sh  --installcert $installcert \
+                    --keypath $keypath${allDomian[0]}.key \
+                    --certpath $certpath${allDomian[0]}.cer \
+                    --reloadcmd "$reloadcmd"
--- acme_dns.sh
+++ acme_dns.sh
@@ -2,7 +2,6 @@
 
 GD_Key=$1
 GD_Secret=$2
-Domains=""
 
 for i in `seq 3 $#`;do
   Domains+=" -d $3"
@@ -35,13 +34,7 @@ echo -e "\033[32mInstall acme.sh.......\033[0m"
 curl https://get.acme.sh | bash
 
 echo -e "\033[32mCreate SSL CRT.......\033[0m"
-~/.acme.sh/acme.sh   --issue   --dns dns_GD -d mritd.me -d www.mritd.me -d cdn.mritd.me
-
-echo -e "\033[32mCopy SSL CRT......\033[0m"
-~/.acme.sh/acme.sh  --installcert $Domains \
-                    --keypath /etc/nginx/ssl/mritd.me.key \
-                    --certpath /etc/nginx/ssl/mritd.me.cer \
-                    --reloadcmd "cd /root/docker/mritd && docker-compose restart"
+~/.acme.sh/acme.sh --issue --dns dns_GD $Domains
 
 echo -e "\033[32mUpdate acme.sh......\033[0m"
 ~/.acme.sh/acme.sh  --upgrade  --auto-upgrade
