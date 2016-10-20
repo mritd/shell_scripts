4c33820d3926be054a3c8bb477d0db7471244345
mritd <mritd1234@gmail.com>
2016-10-20 22:42:44 +0800

    修改证书生成

--- acme_cert_install.sh
+++ acme_cert_install.sh
@@ -3,23 +3,20 @@
 shellName=`ps | grep $$ | awk '{print $4}'`
 
 if [ "$shellName" == "zsh" ]; then
-  read "keypath?请输入 keypath 目录: "
-  read "certpath?请输入 certpath 目录: "
+  read "cerpath?请输入证书存放目录: "
   read "domains?请输入域名(多个以空格分割): "
   read "reloadcmd?请输入重载命令(非必选): "
 else
-  read -p "请输入 keypath 目录: " keypath
-  read -p "请输入 certpath 目录: " certpath
+  read -p "请输入证书存放目录: " cerpath
   read -p "请输入域名(多个以空格分割): " domains
   read -p "请输入重载命令(非必选): " reloadcmd
 fi
 
-if [ "$keypath" == "" ] ||  [ "$certpath" == "" ] ||  [ "$domains" == "" ]; then
-  echo -e "\033[31merror: keypath、certpath or domains is blank!"
+if [ "$cerpath" == "" ] ||  [ "$domains" == "" ]; then
+  echo -e "\033[31merror: cerpath or domains is blank!"
   exit 1
 else
-  mkdir -p $keypath
-  mkdir -p $certpath
+  mkdir -p $cerpath
 fi
 
 allDomian=($domains)
@@ -29,6 +26,7 @@ for domainName in ${allDomian[@]}; do
 done
 
 ~/.acme.sh/acme.sh  --installcert $installcert \
-                    --keypath $keypath/${allDomian[0]}.key \
-                    --certpath $certpath/${allDomian[0]}.cer \
+                    --keypath $cerpath/${allDomian[0]}.key \
+                    --capath $cerpath/ca.cer \
+                    --fullchainpath $cerpath/${allDomian[0]}.cer \
                     --reloadcmd "$reloadcmd"
