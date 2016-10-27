907eeab47161ed6485d34605baffde42b0dfab82
mritd <mritd@mritd.me>
2016-10-27 23:33:30 +0900

    复制 rpm 包到仓库

--- signature_rpm.sh
+++ signature_rpm.sh
@@ -4,4 +4,5 @@ echo "%_gpg_name mritd (mritd rpm repository)" >> ~/.rpmmacros
 
 for rpmName in `ls ~/kubeadm_release/rpm/output/x86_64/*.rpm`; do
   rpm --addsign $rpmName
+  cp ~/kubeadm_release/rpm/output/x86_64/$rpmName /data/repo/centos/7/x86_64
 done
