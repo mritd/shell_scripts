66c02da4afdb4f5562737796354c4c58dc85fe66
mritd <mritd@mritd.me>
2016-11-10 15:52:29 +0900

    增加编译动作

--- build_signature_rpm.sh
+++ build_signature_rpm.sh
@@ -1,8 +1,10 @@
 #/bin/bash
-echo %_signature gpg > ~/.rpmmacros
-echo "%_gpg_name mritd (mritd rpm repository)" >> ~/.rpmmacros
 
 rm -f  ~/kubeadm_release/rpm/output/x86_64/*.rpm
+(cd ~/kubeadm_release/rpm && bash docker-build.sh)
+
+echo %_signature gpg > ~/.rpmmacros
+echo "%_gpg_name mritd (mritd rpm repository)" >> ~/.rpmmacros
 
 for rpmName in `ls ~/kubeadm_release/rpm/output/x86_64/*.rpm`; do
   rpm --addsign $rpmName
