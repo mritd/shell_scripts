b52e92d7148b5775dbf46a6cf2877e6c56aedf03
mritd <mritd@mritd.me>
2016-11-10 14:49:52 +0900

    增加清理动作

--- signature_rpm.sh
+++ signature_rpm.sh
@@ -2,6 +2,8 @@
 echo %_signature gpg > ~/.rpmmacros
 echo "%_gpg_name mritd (mritd rpm repository)" >> ~/.rpmmacros
 
+rm -f  ~/kubeadm_release/rpm/output/x86_64/*.rpm
+
 for rpmName in `ls ~/kubeadm_release/rpm/output/x86_64/*.rpm`; do
   rpm --addsign $rpmName
   cp -f $rpmName /data/repo/centos/7/x86_64
