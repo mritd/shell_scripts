a41ad2ed52e6151e9ff99f30b56d7b4ea9604676
mritd <mritd@mritd.me>
2016-11-10 14:49:05 +0900

    修正 rpm 路径错误

--- signature_rpm.sh
+++ signature_rpm.sh
@@ -4,7 +4,7 @@ echo "%_gpg_name mritd (mritd rpm repository)" >> ~/.rpmmacros
 
 for rpmName in `ls ~/kubeadm_release/rpm/output/x86_64/*.rpm`; do
   rpm --addsign $rpmName
-  cp -f ~/kubeadm_release/rpm/output/x86_64/$rpmName /data/repo/centos/7/x86_64
+  cp -f $rpmName /data/repo/centos/7/x86_64
 done
 
 docker exec -it mritd_rpm_1 /root/flush.sh
