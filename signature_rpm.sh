5c7a08b44e7db13cc2de91cba67921dc4ff08165
mritd <mritd@mritd.me>
2016-11-13 18:55:42 +0900

    update

--- signature_rpm.sh
+++ signature_rpm.sh
@@ -1,8 +1,6 @@
 #/bin/bash
 
-rm -f  ~/kubeadm_release/rpm/output/x86_64/*.rpm
 rm -f /data/repo/centos/7/x86_64/{kubelet*,kubeadm*,kubernetes-cni*,kubectl*}
-(cd ~/kubeadm_release/rpm && git pull && bash docker-build.sh)
 
 echo %_signature gpg > ~/.rpmmacros
 echo "%_gpg_name mritd (mritd rpm repository)" >> ~/.rpmmacros
