75628fbb479ab3417671dfc4d1a2a35a64ad19be
mritd <mritd@mritd.me>
2016-11-14 09:51:56 +0900

    update

--- download_kube_image.sh
+++ download_kube_image.sh
@@ -2,7 +2,7 @@
 
 # This script is used to download some images and packaged kubernetes used
 
-images=(kube-proxy-amd64:v1.4.0 kube-discovery-amd64:1.0 kubedns-amd64:1.7 kube-scheduler-amd64:v1.4.0 kube-controller-manager-amd64:v1.4.0 kube-apiserver-amd64:v1.4.0 etcd-amd64:2.2.5 kube-dnsmasq-amd64:1.3 exechealthz-amd64:1.1 pause-amd64:3.0)
+images=(kube-proxy-amd64:v1.4.5 kube-discovery-amd64:1.0 kubedns-amd64:1.7 kube-scheduler-amd64:v1.4.5 kube-controller-manager-amd64:v1.4.5 kube-apiserver-amd64:v1.4.5 etcd-amd64:2.2.5 kube-dnsmasq-amd64:1.3 exechealthz-amd64:1.1 pause-amd64:3.0 kubernetes-dashboard-amd64:v1.4.2)
 
 echo -e "\033[33mclean old files!\033[0m"
 rm -rf ~/kube_images > /dev/null 2>&1
