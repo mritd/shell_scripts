0537dd9e291f3949a3a30b2ca9cc50cf2ca2c021
mritd <mritd@mritd.me>
2016-12-13 15:58:10 +0900

    修复目录错误

--- syncrpm.sh
+++ syncrpm.sh
@@ -8,6 +8,6 @@ upx rm centos/7/x86_64/repodata/\*
 sleep 120
 
 # sync rpm
-cd /data/centos/7/x86_64/ && for rpmName in `ls *.rpm`;do upx put $rpmName centos/7/x86_64/;done
-cd /data/centos/7/x86_64/repodata/ && for repodata in `ls`;do upx put $repodata centos/7/x86_64/repodata/;done
+cd /data/repo/centos/7/x86_64/ && for rpmName in `ls *.rpm`;do upx put $rpmName centos/7/x86_64/;done
+cd /data/repo/centos/7/x86_64/repodata/ && for repodata in `ls`;do upx put $repodata centos/7/x86_64/repodata/;done
 
