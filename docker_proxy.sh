115e46a0be425b9d20f30260f26cdaf5aa0d4cc1
mritd <mritd1234@gmail.com>
2017-01-11 08:17:56 +0800

    切换到 socks5

--- docker_proxy.sh
+++ docker_proxy.sh
@@ -4,7 +4,7 @@ mkdir /etc/systemd/system/docker.service.d
 
 tee /etc/systemd/system/docker.service.d/http-proxy.conf <<EOF
 [Service]
-Environment="HTTP_PROXY=http://192.168.1.120:8123"
+Environment="ALL_PROXY=socks5://192.168.1.120:1083"
 EOF
 
 systemctl daemon-reload
