c51aa3a650a86730939b84822d4e2f7b4537523d
mritd <mritd@mritd.me>
2017-02-26 11:35:20 +0000

    增加初始化安装包

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum update -y
-yum install tmux wget lrzsz vim net-tools zsh bind-utils -y
+yum install tmux wget lrzsz vim net-tools zsh bind-utils yum-utils catgs -y
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
