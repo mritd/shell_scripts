4aa967f54e555fd828649a8f8d94cac3a3fb021b
mritd <mritd1234@gmail.com>
2017-03-04 18:26:57 +0800

    排除标题

--- docker_clean.sh
+++ docker_clean.sh
@@ -2,4 +2,4 @@
 
 docker ps -a | grep "Exited" | awk '{print $1 }'|xargs docker stop
 docker ps -a | grep "Exited" | awk '{print $1 }'|xargs docker rm
-docker images|grep none|awk '{print $3 }'|xargs docker rmi
+docker images | grep none | grep -v REPOSITORY | awk '{print $3 }' | xargs docker rmi
