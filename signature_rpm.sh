617240b1b6e3d44828ead233aa27719d4f9e21f4
mritd <mritd@mritd.me>
2017-03-10 07:12:33 +0000

    更新签名脚本

--- signature_rpm.sh
+++ signature_rpm.sh
@@ -3,7 +3,7 @@
 yum install rpm-sign -y 
 
 # clean old files
-rm -f /data/repo/centos/7/x86_64/{kubelet*,kubeadm*,kubernetes-cni*,kubectl*}
+#rm -f /data/repo/centos/7/x86_64/{kubelet*,kubeadm*,kubernetes-cni*,kubectl*}
 
 # signature rpms
 echo %_signature gpg > ~/.rpmmacros
@@ -15,7 +15,7 @@ for rpmName in `ls ~/git_projects/adm_release/rpm/output/x86_64/*.rpm`; do
 done
 
 # create repodata
-docker exec -it mritd_rpm_1 /root/flush.sh
+`pwd`/flush_repo.sh
 
 # sync cdn
 `pwd`/syncrpm.sh
