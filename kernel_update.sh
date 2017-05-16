cdc3e54b4b65dc8adcef648596c921098f8ee702
mritd <mritd1234@gmail.com>
2017-05-16 12:50:58 +0800

    增加  kernel-lt-headers

--- kernel_update.sh
+++ kernel_update.sh
@@ -7,7 +7,7 @@ rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm
 
 # install kernel
-yum --enablerepo=elrepo-kernel install  kernel-lt-devel kernel-lt -y
+yum --enablerepo=elrepo-kernel install kernel-lt kernel-lt-headers kernel-lt-devel -y
 
 # modify grub
 grub2-set-default 0
