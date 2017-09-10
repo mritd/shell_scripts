e00e898e3124cc9ba7228733a0817acb2fcc7e27
mritd <mritd1234@gmail.com>
2017-09-10 10:58:04 +0800

    支持动态仓库地址

--- create_imagePullSecret.sh
+++ create_imagePullSecret.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
 REGISTRY_TOKEN=`sudo cat ~/.docker/config.json | base64 | tr -d '\n'`
+REGISTRY_ADDRESS=$1
+
+if [ "" == $REGISTRY_ADDRESS ]
+    echo -e "\033[31mError: REGISTRY_ADDRESS is blank!\033[0m"
+    exit 1
+fi
+
 
 tee imagePullSecret.yaml <<EOF
 apiVersion: v1
 kind: Secret
 metadata:
-  name: reg.domain.com 
+  name: $REGISTRY_ADDRESS 
   namespace: default
 data:
   .dockerconfigjson: $REGISTRY_TOKEN
 type: kubernetes.io/dockerconfigjson
 EOF
 kubectl create -f imagePullSecret.yaml
-kubectl patch serviceaccounts default -p '{"imagePullSecrets":[{"name":"reg.domain.com"}]}'
+kubectl patch serviceaccounts default -p "{\"imagePullSecrets\":[{\"name\":\"$REGISTRY_ADDRESS\"}]}"
