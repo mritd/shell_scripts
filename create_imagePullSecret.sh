479972e4b4ac46a5c988288522b134dba1ee5944
mritd <mritd1234@gmail.com>
2017-09-10 10:55:23 +0800

    增加仓库地址样例

--- create_imagePullSecret.sh
+++ create_imagePullSecret.sh
@@ -6,11 +6,11 @@ tee imagePullSecret.yaml <<EOF
 apiVersion: v1
 kind: Secret
 metadata:
-  name: imagePullSecrets
+  name: reg.domain.com 
   namespace: default
 data:
   .dockerconfigjson: $REGISTRY_TOKEN
 type: kubernetes.io/dockerconfigjson
 EOF
 kubectl create -f imagePullSecret.yaml
-kubectl patch serviceaccounts default -p '{"imagePullSecrets":[{"name":"imagePullSecret"}]}'
+kubectl patch serviceaccounts default -p '{"imagePullSecrets":[{"name":"reg.domain.com"}]}'
