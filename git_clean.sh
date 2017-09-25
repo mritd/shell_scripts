56d34baca6ce1ff68945866531525da205ea9b0b
mritd <mritd1234@gmail.com>
2017-09-25 15:38:27 +0800

    增加 git 大文件清理脚本

---
+++ git_clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git rev-list --objects --all | grep "$(git verify-pack -v .git/objects/pack/*.idx | sort -k 3 -n | tail -5 | awk '{print$1}')"
+
+git filter-branch --force --index-filter 'git rm -rf --cached --ignore-unmatch BIGFILE' --prune-empty --tag-name-filter cat -- --all
+
+git push origin master --force
+
+rm -rf .git/refs/original/
+
+git reflog expire --expire=now --all
+
+git gc --prune=now
