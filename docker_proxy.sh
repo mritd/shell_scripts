72ebed666d1bdd47172a6a3b25b96973f1a014e7
mritd <mritd1234@gmail.com>
2017-11-20 20:12:54 +0800

    :sparkles: feat(docker proxy): 增加 socks5 代理地址设置
    增加 socks5 代理地址设置
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- docker_proxy.sh
+++ docker_proxy.sh
@@ -3,22 +3,29 @@
 set -e
 
 OS_TYPE=$1
+PROXY_ADDRESS=$2
 
-if [ "$OS_TYPE" == "" ];then
+if [ "${PROXY_ADDRESS}" == "" ]; then
+    echo -e "\033[31mError: PROXY_ADDRESS is blank!\033[0m"
+    echo -e "\033[32mUse: sudo $0 centos|ubuntu 1.2.3.4:1080\033[0m"
+    exit 1
+fi
+
+if [ "${OS_TYPE}" == "" ];then
     echo -e "\033[31mError: OS_TYPE is blank!\033[0m"
     echo -e "\033[32mUse: sudo $0 centos|ubuntu\033[0m"
     exit 1
-elif [ "$OS_TYPE" == "centos" ];then
+elif [ "${OS_TYPE}" == "centos" ];then
     mkdir /etc/systemd/system/docker.service.d || true
     tee /etc/systemd/system/docker.service.d/socks5-proxy.conf <<-EOF
 [Service]
-Environment="ALL_PROXY=socks5://192.168.1.21:1080"
+Environment="ALL_PROXY=socks5://${PROXY_ADDRESS}"
 EOF
-elif [ "$OS_TYPE" == "ubuntu" ];then
+elif [ "${OS_TYPE}" == "ubuntu" ];then
     mkdir /lib/systemd/system/docker.service.d || true
     tee /lib/systemd/system/docker.service.d/socks5-proxy.conf <<-EOF
 [Service]
-Environment="ALL_PROXY=socks5://192.168.1.21:1083"
+Environment="ALL_PROXY=socks5://${PROXY_ADDRESS}"
 EOF
 fi
 
