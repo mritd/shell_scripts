2d1ba28104d8deac8ff355e78b65ed66c22dec7b
mritd <mritd1234@gmail.com>
2017-11-20 10:56:44 +0800

    :sparkles: feat(imagePullSecret): 增加 namesapce 获取
    增加 namesapce 获取
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- create_imagePullSecret.sh
+++ create_imagePullSecret.sh
@@ -2,8 +2,9 @@
 
 REGISTRY_TOKEN=`sudo cat ~/.docker/config.json | base64 | tr -d '\n'`
 REGISTRY_ADDRESS=$1
+NAMESPACE=${NAMESAPCE:-"default"}
 
-if [ "" == $REGISTRY_ADDRESS ]; then
+if [ "" == ${REGISTRY_ADDRESS} ]; then
     echo -e "\033[31mError: REGISTRY_ADDRESS is blank!\033[0m"
     exit 1
 fi
@@ -13,11 +14,11 @@ tee imagePullSecret.yaml <<EOF
 apiVersion: v1
 kind: Secret
 metadata:
-  name: $REGISTRY_ADDRESS 
-  namespace: default
+  name: ${REGISTRY_ADDRESS} 
+  namespace: ${NAMESPACE}
 data:
-  .dockerconfigjson: $REGISTRY_TOKEN
+  .dockerconfigjson: ${REGISTRY_TOKEN}
 type: kubernetes.io/dockerconfigjson
 EOF
 kubectl create -f imagePullSecret.yaml
-kubectl patch serviceaccounts default -p "{\"imagePullSecrets\":[{\"name\":\"$REGISTRY_ADDRESS\"}]}"
+kubectl patch serviceaccounts default -p "{\"imagePullSecrets\":[{\"name\":\"${REGISTRY_ADDRESS}\"}]}"
