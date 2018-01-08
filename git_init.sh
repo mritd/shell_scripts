649f1f4b3c6cf13ff5bdb7c8c7abecd72b2045db
mritd <mritd1234@gimail.com>
2018-01-08 12:51:36 +0800

    :sparkles: feat(git init): 调整 push 模式为 simple
    调整 push 模式为 simple
    
    Signed-off-by: mritd <mritd1234@gimail.com>

--- git_init.sh
+++ git_init.sh
@@ -3,4 +3,5 @@
 git config --global user.name mritd
 git config --global user.email mritd1234@gimail.com
 git config --global core.editor vim
+git config --global push.default simple
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/tonydeng/git-toolkit/master/installer.sh)"
