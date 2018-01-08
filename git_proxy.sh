deea5d2db86937960314f5948aa4c96391317c68
mritd <mritd1234@gimail.com>
2018-01-08 13:03:06 +0800

    :sparkles: feat(git proxy): 调整 git 代理脚本
    调整 git 代理脚本
    
    Signed-off-by: mritd <mritd1234@gimail.com>

--- git_proxy.sh
+++ git_proxy.sh
@@ -3,14 +3,14 @@
 tee /usr/local/bin/proxy-wrapper <<EOF
 #!/bin/bash
 #nc -x192.168.1.21:1083 -X5 \$*
-connect-proxy -S 192.168.1.21:1083 $*
+connect-proxy -S 192.168.1.21:1083 \$*
 EOF
 
 chmod +x /usr/local/bin/proxy-wrapper
 
 tee ~/.ssh/config <<EOF
 Host github github.com mritd.me
-#Hostname github.com
-#User git
-ProxyCommand /usr/local/bin/proxy-wrapper '%h %p'
+    #Hostname github.com
+    #User git
+    ProxyCommand /usr/local/bin/proxy-wrapper '%h %p'
 EOF
