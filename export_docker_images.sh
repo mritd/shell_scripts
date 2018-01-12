8ab81ba1f897e232b9638343db983ee0b170502b
mritd <mritd1234@gmail.com>
2018-01-12 11:12:37 +0800

    :sparkles: feat(images): 增加单镜像导出
    增加单镜像导出
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- export_docker_images.sh
+++ export_docker_images.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-for imageName in `docker images | grep -v "REPOSITORY" | awk '{print $1":"$2}'`;do docker save $imageName > `echo $imageName | tr '/' '_' | tr ':' '_'`.tar ; done
+
+set -e
+
+if [ "$1" == "" ]; then
+    echo -e "\033[31mError: imageName is blank!\033[0m"
+    exit 1
+elif [ "$1" == "all" ]; then
+    for imageName in `docker images | grep -v "REPOSITORY" | awk '{print $1":"$2}'`; do
+        docker save $imageName > `echo $imageName | tr '/' '_' | tr ':' '_'`.tar
+    done
+else
+    docker save $1 > `echo $1 | tr '/' '_' | tr ':' '_'`.tar
+fi
