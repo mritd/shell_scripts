d3743774537b35c48807c6732ec563008d578427
mritd <mritd1234@gmail.com>
2018-01-17 13:37:36 +0800

    :sparkles: feat(kubectl config): 增加配置文件名
    增加配置文件名
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- generate_kubectl_config.sh
+++ generate_kubectl_config.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 KUBE_API_SERVER=$1
-CERT_DIR=${CERT_DIR-"."}
+CERT_DIR=${2:-"/etc/kubernetes/ssl"}
 
 kubectl config set-cluster default-cluster --server=${KUBE_API_SERVER} \
-    --certificate-authority=${CERT_DIR}/k8s-root-ca.pem
+    --certificate-authority=${CERT_DIR}/k8s-root-ca.pem \
+    --kubeconfig=admin.kubeconfig
 
 kubectl config set-credentials default-admin \
     --certificate-authority=${CERT_DIR}/k8s-root-ca.pem \
+    --embed-certs=true \
     --client-key=${CERT_DIR}/admin-key.pem \
-    --client-certificate=${CERT_DIR}/admin.pem      
+    --client-certificate=${CERT_DIR}/admin.pem \
+    --kubeconfig=admin.kubeconfig 
 
-kubectl config set-context default-system --cluster=default-cluster --user=default-admin
-kubectl config use-context default-system
+kubectl config set-context default-system --cluster=default-cluster \
+    --user=default-admin \
+    --kubeconfig=admin.kubeconfig
+
+kubectl config use-context default-system --kubeconfig=admin.kubeconfig
