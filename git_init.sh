2dbdc093f45764fbb753561c89db3e8b128c6338
mritd <mritd1234@gmail.com>
2018-03-08 20:00:41 +0800

    :sparkles: feat(git init): 添加全局 git 排除
    添加全局 git 排除
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- git_init.sh
+++ git_init.sh
@@ -4,4 +4,6 @@ git config --global user.name mritd
 git config --global user.email mritd1234@gmail.com
 git config --global core.editor vim
 git config --global push.default simple
+git config --global core.excludesfile ~/.global_gitignore
+echo ".idea" >> ~/.global_gitignore
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/tonydeng/git-toolkit/master/installer.sh)"
