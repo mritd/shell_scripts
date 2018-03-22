f9e3508c241ec837cb4b7960fa1739fd89fa2aa2
mritd <mritd1234@gmail.com>
2018-03-22 11:47:19 +0800

    feat(git gpg): add gpg support
    add gpg support
    
    Signed-off-by: mritd <mritd1234@gmail.com>

--- git_init.sh
+++ git_init.sh
@@ -5,5 +5,8 @@ git config --global user.email mritd1234@gmail.com
 git config --global core.editor vim
 git config --global push.default simple
 git config --global core.excludesfile ~/.global_gitignore
+git config --global user.signingkey 1C78FD62CEA2C04B79EA459F7CB6F1DA9030B819
+git config --global commit.gpgsign true
+git config --global gpg.program gpg
+git config --global core.quotepath false
 echo ".idea" >> ~/.global_gitignore
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/tonydeng/git-toolkit/master/installer.sh)"
