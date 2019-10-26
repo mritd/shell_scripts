a896986493aac2e9a7b2730bcdae9bcbbb50ee4b
mritd <mritd@linux.com>
2019-10-26 15:03:57 +0800

    feat(all): clean
    clean
    
    Signed-off-by: mritd <mritd@linux.com>

--- backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf /backup >& /dev/null &&  mkdir /backup
-
-cd / && tar -zcvf root.tar.gz root && mv root.tar.gz /backup
-cd / && tar -zcvf data.tar.gz data && mv data.tar.gz /backup
-cd /etc && tar -zcvf nginx.tar.gz nginx && mv nginx.tar.gz /backup
--- mac_ntfs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-brew cask install osxfuse
-brew install ntfs-3g
-
-sudo mv /sbin/mount_ntfs /sbin/mount_ntfs.bak
-sudo ln -s /usr/local/bin/ntfs-3g /sbin/mount_ntfs
