9a9d39a5c2e989ccb65644e8a846c0f414be2bbc
mritd <mritd@linux.com>
2020-03-30 11:48:41 +0800

    feat(coredns): add install_coredns
    add install_coredns
    
    Signed-off-by: mritd <mritd@linux.com>

--- install_coredns.sh
+++ install_coredns.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
 
 set -e
 
 VERSION="${1}"
 
 if [ -z "${VERSION}" ]; then
-    VERSION="1.6.4"
-    echo "No CoreDNS version specified, use default version: 1.6.4!"
+    VERSION="1.6.9"
+    echo "No CoreDNS version specified, use default version: ${VERSION}!"
 fi
 
 COREDNS_URL="https://github.com/coredns/coredns/releases/download/v${VERSION}/coredns_${VERSION}_linux_amd64.tgz"
@@ -15,18 +14,22 @@ SYSUSERS_CONF="https://raw.githubusercontent.com/coredns/deployment/master/syste
 TEMPFILES_CONF="https://raw.githubusercontent.com/coredns/deployment/master/systemd/coredns-tmpfiles.conf"
 SERVICE_CONF="https://raw.githubusercontent.com/coredns/deployment/master/systemd/coredns.service"
 
-curl -fsSL ${COREDNS_URL} > coredns.tar.gz
-curl -fsSL ${SYSUSERS_CONF} > /usr/lib/sysusers.d/coredns-sysusers.conf
-curl -fsSL ${TEMPFILES_CONF} > /usr/lib/tmpfiles.d/coredns-tmpfiles.conf
-curl -fsSL ${SERVICE_CONF} > /lib/systemd/system/coredns.service
+curl -sSL ${COREDNS_URL} > coredns.tar.gz
+curl -sSL ${SYSUSERS_CONF} > /usr/lib/sysusers.d/coredns-sysusers.conf
+curl -sSL ${TEMPFILES_CONF} > /usr/lib/tmpfiles.d/coredns-tmpfiles.conf
+curl -sSL ${SERVICE_CONF} > /lib/systemd/system/coredns.service
 
-tar -zxf coredns.tar.gz --strip-components=1 -C /usr/bin
+#tar -zxf coredns.tar.gz --strip-components=1 -C /usr/bin
+tar -zxf coredns.tar.gz -C /usr/bin
 systemd-sysusers
 systemd-tmpfiles --create
 systemctl daemon-reload
 
-mkdir -p /etc/coredns
-curl -fsSL ${COREDNS_CONF} > /etc/coredns/Corefile
+if [ ! -d "/etc/coredns" ]; then
+    mkdir -p /etc/coredns
+fi
+
+curl -sSL ${COREDNS_CONF} > /etc/coredns/Corefile
 touch /etc/coredns/hosts
 
 rm -f coredns.tar.gz
